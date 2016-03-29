89ae7fd1467e3da8564bfa541d013c7cecb61097
dbassols <zeus@stucom.com>
2016-03-29 21:31:25 +0200

    gfhfhg

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,5 +1,5 @@
 public class HolaMundo {
-	private static String nombre = "Cristina";
+	private static String nombre = "David";
 	public static void main (String[]args){
 		System.out.println("hola");
 		System.out.println(nombre);
